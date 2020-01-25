ccf5bb8e1d61c9ac37e9b2e44d6a02869f1019e3
naho <nd@v7.com>
2020-01-25 03:54:24 +0000

    9.1.2　バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
@@ -34,13 +35,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-    # トークンがダイジェストと一致すればtrueを返します。
+  # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
     # ダイジェストが存在しない場合はfalseを返して終了します。
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
+
+  # ユーザーのログイン情報を破棄します。
   def forget
     update_attribute(:remember_digest, nil)
   end
